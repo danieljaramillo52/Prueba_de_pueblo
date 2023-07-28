8da9c8d3680db05b81299c78a09442e858cd11e1
danieljaramillo52 <133677401+danieljaramillo52@users.noreply.github.com>
2023-07-28 16:21:01 -0500

    Cambios guardados

---
+++ app/src/main/java/com/example/vengaamipueblo3/adaptadores/HotelAdaptador.java
@@ -0,0 +1,63 @@
+package com.example.vengaamipueblo3.adaptadores;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.vengaamipueblo3.R;
+import com.example.vengaamipueblo3.moldes.MoldeHotel;
+
+import java.util.ArrayList;
+
+public class HotelAdaptador extends RecyclerView.Adapter<HotelAdaptador.viewHolder>{
+
+    //ATRIBUTOS
+    public ArrayList<MoldeHotel> listaHoteles;
+
+    //CONSTRUCTOR
+
+    public HotelAdaptador(ArrayList<MoldeHotel> listaHoteles) {
+        this.listaHoteles = listaHoteles;
+    }
+
+    @NonNull
+    @Override
+    public HotelAdaptador.viewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View vista = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.moldehotel,null,false);
+                return new viewHolder(vista) ;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HotelAdaptador.viewHolder viewHolder, int i) {
+        viewHolder.actualizarDatos(listaHoteles.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaHoteles.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+        TextView nombreHotel;
+        TextView precioHotel;
+        ImageView fotodelHotel;
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            fotodelHotel= itemView.findViewById(R.id.fotoitem);
+            nombreHotel= itemView.findViewById(R.id.tituloitem);
+            precioHotel = itemView.findViewById(R.id.precioitem);
+        }
+
+        public void actualizarDatos(MoldeHotel moldeHotel) {
+            fotodelHotel.setImageResource(moldeHotel.getFoto());
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/vengaamipueblo3/moldes/MoldeHotel.java
@@ -0,0 +1,39 @@
+package com.example.vengaamipueblo3.moldes;
+
+public class MoldeHotel {
+
+        private String precio;
+        private int foto;
+        private  String nombre;
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public MoldeHotel (String nombre, String precio, int foto){
+
+        }
+
+
+
+}
